edd937fec7d47ee3163432e7432020a597929220
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-02-28 21:32:15 -0500

    Project 1: added log4j dependencies to pom, added oracle rds connection

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/util/ConnectionFactory.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/util/ConnectionFactory.java
@@ -1,5 +1,55 @@
 package com.revature.ers.util;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 public class ConnectionFactory {
+
+	private static Logger log = Logger.getLogger(ConnectionFactory.class);
+	private static ConnectionFactory cf = new ConnectionFactory();
+	
+	private ConnectionFactory() {
+		super();
+	}
+	
+	public static ConnectionFactory getInstance() {
+		return cf;
+	}
+	
+	public Connection getConnection() {
+		
+		Connection conn = null;
+		
+		// We use a .properties file so we do not hard-code our DB credentials into the source code
+		Properties prop = new Properties();
+		
+		try {
+			
+			// Load the properties file (application.properties) keys/values into the Properties object
+			prop.load(new FileReader("src/main/resources/application.properties"));
+			
+			// Get a connection from the DriverManager class
+			conn = DriverManager.getConnection(
+					prop.getProperty("url"),
+					prop.getProperty("usr"),
+					prop.getProperty("pw"));
+			
+		} catch (SQLException sqle) {
+			log.error(sqle.getMessage());
+		} catch (FileNotFoundException fnfe) {
+			log.error(fnfe.getMessage());
+		} catch (IOException ioe) {
+			log.error(ioe.getMessage());
+		}
+		
+		return conn;
+	}
 
 }
