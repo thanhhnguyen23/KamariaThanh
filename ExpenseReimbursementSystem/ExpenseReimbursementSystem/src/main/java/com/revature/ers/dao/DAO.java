1d53a74020acbd4a3adc12e2a81e41d2bdb456c8
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-02 12:51:31 -0500

    Project 1: creating triggers, inserting dummy values, and sequences

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/dao/DAO.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/dao/DAO.java
@@ -1,5 +1,13 @@
 package com.revature.ers.dao;
 
-public interface DAO {
+import java.util.List;
 
+public interface DAO<T> {
+	
+	List<T> getAll();
+	T getById(int id);
+	T add(T obj);
+	T update(T updatedObj);
+	boolean delete(int id);
+	
 }
--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/resources/ers_table_creation.sql
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/resources/ers_table_creation.sql
@@ -1,5 +1,12 @@
 
 -- should the users (employee/manager) be able to sign up with the system?
+	-- up for discussion on various methods 
+	-- templates vs angular templates
+	-- servlets handling register/login logic
+	
+-- ************************************************
+-- table creation
+-- ************************************************
 CREATE TABLE ers_users(
 	ers_user_id 		NUMBER,
 	ers_username		VARCHAR2(50) UNIQUE,
@@ -50,9 +57,8 @@ CREATE TABLE ers_reimbursement(
 	FOREIGN KEY (reimb_resolver)
 	REFERENCES ers_users_roles (ers_user_role_id)  
 	-- typical roles would be employee (1) and manager (2)
-	-- resolver must be a specific type of user? 
-	--how do you know this user has authorization to "resolve"
-
+	-- resolver will be the user tied to a specific user_role_id defining the "privileges"
+	
 	CONSTRAINT ers_reimbursement_status_fk
 	FOREIGN KEY (reimb_status_id)
 	REFERENCES ers_reimbursement_status (reimb_status_id)
@@ -80,3 +86,85 @@ CREATE TABLE ers_reimbursement_type(
 	CONSTRAINT reimb_type_pk
 	PRIMARY (reimb_type_id)
 );
+
+-- ************************************************
+-- trigger creation
+-- ************************************************
+CREATE OR REPLACE TRIGGER ers_users_pk_trigger BEFORE
+    INSERT ON ers_users
+    FOR EACH ROW
+BEGIN
+    SELECT
+        ers_users_pk_seq.NEXTVAL
+    INTO :new.ers_user_id
+    FROM
+        dual;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER ers_roles_pk_trigger BEFORE
+    INSERT ON ers_user_roles
+    FOR EACH ROW
+BEGIN
+    SELECT
+        ers_user_roles_pk_seq.NEXTVAL
+    INTO :new.user_role
+    FROM
+        dual;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER ers_reimb_pk_trigger BEFORE
+    INSERT ON ers_reimbursement
+    FOR EACH ROW
+BEGIN
+    SELECT
+        ers_reimb_id_pk_seq.NEXTVAL
+    INTO :new.reimb_id
+    FROM
+        dual;
+END;
+/
+
+
+-- ************************************************
+-- sequence creation
+-- we'll come back to the status and type
+-- ************************************************
+CREATE SEQUENCE ers_users_pk_seq 
+MINVALUE 1
+MAXVALUE 999999999
+INCREMENT BY 1
+START WITH 1;
+
+CREATE SEQUENCE ers_user_roles_pk_seq
+MINVALUE 1
+MAXVALUE 999999999
+INCREMENT BY 1
+START WITH 1;
+
+CREATE SEQUENCE ers_reimb_id_pk_seq
+MINVALUE 1
+MAXVALUE 999999999
+INCREMENT BY 1
+START WITH 1;
+
+-- ************************************************
+-- populating ers_user_roles tables 
+-- ************************************************
+INSERT INTO ers_user_roles(1, 'ADMIN');
+INSERT INTO ers_user_roles(2, 'EMPLOYEE');
+
+-- ************************************************
+-- populating tables 
+-- ************************************************
+INSERT INTO ers_users VALUES(
+
+	0, 'admin_kd', 'unicorn', 'kamaria', 'deramus', 'kderamus@gmail.com', ''
+--	ers_user_id --	ers_username --	ers_password--	user_first_name --	user_last_name --	user_email--	user_role_id		
+
+
+)
+
