c8b24595befd8f0b34fb99d0eae71ce443658b1f
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-04 16:50:28 -0500

    made minor changes to the userService layer

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/UserService.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/UserService.java
@@ -1,5 +1,37 @@
 package com.revature.ers.services;
 
+import java.util.List;
+
+import com.revature.ers.dao.UserDao;
+import com.revature.ers.models.User;
+
 public class UserService {
+	
+	private UserDao userDao = new UserDao();
+	
+	
+	public List<User> getAllUsers() {
+		return userDao.getAll();
+	}
+	
+	
+	public User getUserByCredentials(String username, String password) {
+		if (!username.equals("") && !password.equals("")) {
+			return userDao.getByCredentials(username, password);
+		}
+		return null;
+	}
+	
+	public User getUserByUsername(String username) {
+		if (!username.equals("")) {
+			return userDao.getByUsername(username);
+		}
+		return null;
+	}
+	
+	//figure out way to get the current user and then that current user's id
+	public User getUserById(int userId) {
+		if (userDao.get)
+	}
 
 }
