b072da092055d61ad74ff7a34df1bc49ff2d9647
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-05 21:42:55 -0500

    Project 1: adding JwtGenerator

---
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/servlets/AuthServlet.java
@@ -0,0 +1,55 @@
+package com.revature.ers.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.revature.ers.models.User;
+import com.revature.ers.services.UserService;
+
+@WebServlet("/auth")
+public class AuthServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static Logger log = Logger.getLogger(AuthServlet.class);
+	
+	private UserService userService = new UserService();
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		ObjectMapper mapper = new ObjectMapper();
+		String[] credentials = null;
+		
+		try {
+			credentials = mapper.readValue(req.getInputStream(), String[].class);
+		} catch (MismatchedInputException mie) {
+			log.error(mie.getMessage());
+			resp.setStatus(400);
+			return;
+		}
+		
+		if(credentials != null && credentials.length != 2) {
+			log.warn("Invalid request, unexpected number of credential values");
+			resp.setStatus(400);
+			return;
+		}
+		
+		User user = userService.getUserByCredentials(credentials[0], credentials[1]);
+		
+		if(user == null) {
+			resp.setStatus(401);
+			return;
+		}
+		
+		resp.setStatus(200);
+		resp.addHeader(JwtConfig.HEADER, JwtConfig.PREFIX + JwtGenerator.createJwt(user));
+	}
+}
