e1c79ed4b9ae1ddb6df8173ff04730b48cbc6f23
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-05 21:00:07 -0500

    Project 1: added methods to ReimbursementService and UserService

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/ReimbursementService.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/ReimbursementService.java
@@ -1,5 +1,45 @@
 package com.revature.ers.services;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.ers.dao.ReimbursementDao;
+import com.revature.ers.models.Reimbursement;
+
 public class ReimbursementService {
+	private static Logger log = Logger.getLogger(ReimbursementService.class);
+	
+	private ReimbursementDao rDao = new ReimbursementDao();
+	
+	public List<Reimbursement> getAllReimbursements(){
+		return rDao.getAll();
+	}
+	
+	public Reimbursement getReimbursementById(int reimbId) {
+		return rDao.getById(reimbId);
+	}
+	
+	public Reimbursement addReimbursement(Reimbursement newReimb) {
+		// get amount
+		if(newReimb.getAmount() != 0 || newReimb.getReimbDescription().equals("") || newReimb.getAuthorId() != 0 || newReimb.getTypeId() != 0) {
+			return null;
+		}
+		return rDao.add(newReimb);
+	}
+
+	public Reimbursement updateReimbursement(Reimbursement updatedReimb) {
+		if(updatedReimb.getAmount() != 0 || updatedReimb.getReimbDescription().equals("") || updatedReimb.getAuthorId() != 0 || updatedReimb.getTypeId() != 0) {
+			return null;
+		}
+		Reimbursement persistedReimb = rDao.update(updatedReimb);
+
+		if (persistedReimb != null) {
+			return updatedReimb;
+		}
+		
+		return null;
+	}
+	
 
 }
--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/UserService.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/services/UserService.java
@@ -2,12 +2,15 @@ package com.revature.ers.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.revature.ers.dao.UserDao;
 import com.revature.ers.models.User;
 
 public class UserService {
 	
 	private UserDao userDao = new UserDao();
+	private static Logger log = Logger.getLogger(UserService.class);
 	
 	public List<User> getAllUsers() {
 		return userDao.getAll();
@@ -30,13 +33,15 @@ public class UserService {
 	
 	//TODO -- look at bookstore v3 for inspiration
 	//figure out way to get the current user and then that current user's id
+
 	//////////////////////////////
 	// things to consider per WS
 	//awt - filter
 	//////////////////////////////
-	
 	public User getUserById(int userId) {
-		if (userDao.get)
+
+		return userDao.getById(userId);
 	}
+	
 
 }
