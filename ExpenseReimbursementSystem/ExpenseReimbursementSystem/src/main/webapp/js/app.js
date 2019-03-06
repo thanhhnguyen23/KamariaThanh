cdb953da18b4242dcade38c12cc888151bc1ad78
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-06 11:43:19 -0500

    pushing front end updates from morning

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/webapp/js/app.js
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/webapp/js/app.js
@@ -1,9 +1,9 @@
-// window.onload = function() {
-//     document.getElementById('to-login').addEventListener('click', loadLogin);
-//     document.getElementById('to-register').addEventListener('click', loadRegister);
-//     document.getElementById('to-dashboard').addEventListener('click', loadDashboard);
-//     document.getElementById('to-logout').addEventListener('click', logout);
-// }
+window.onload = function() {
+    document.getElementById('to-login').addEventListener('click', loadLogin);
+    document.getElementById('to-register').addEventListener('click', loadRegister);
+    document.getElementById('to-dashboard').addEventListener('click', loadDashboard);
+    document.getElementById('to-logout').addEventListener('click', logout);
+}
 
 // /*
 //     Login component
@@ -12,50 +12,46 @@
 //         - configureLogin()
 //         - login()
 // */
-// async function loadLogin() {
-//     console.log('in loadLogin()');
+async function loadLogin() {
+    console.log('in loadLogin()');
 
-//     // fetchView('login.view').then(view => {
-//     //     APP_VIEW.innerHTML = view;
-//     //     DYNAMIC_CSS_LINK.href = 'css/login.css';
-//     //     configureLogin();
-//     // });
-    
-//     APP_VIEW.innerHTML = await fetchView('login.view');
-//     DYNAMIC_CSS_LINK.href = 'css/login.css';
-//     configureLogin();
-// }
+    // changing the view to login
+    APP_VIEW.innerHTML = await fetchView('login.view');
+    DYNAMIC_CSS_LINK.href = 'css/login.css';
+    configureLogin(); //calling configure
+}
 
-// function configureLogin() {
-//     console.log('in configureLogin()');
-//     document.getElementById('alert-msg').hidden = true;
-//     document.getElementById('submit-creds').addEventListener('click', login);
-// }
+// here, we have event listeners to toggle an alert and invoke login() function
+function configureLogin() {
+    console.log('in configureLogin()');
+    document.getElementById('alert-msg').hidden = true;
+    document.getElementById('submit-creds').addEventListener('click', login);
+}
 
-// async function login() {
-//     console.log('in login()');
-//     let credentials = [];
-//     credentials.push(document.getElementById('username-cred').value);
-//     credentials.push(document.getElementById('password-cred').value);
+async function login() {
+    console.log('in login()');
+    let credentials = [];
+    credentials.push(document.getElementById('username-cred').value);
+    credentials.push(document.getElementById('password-cred').value);
 
-//     let response = await fetch('auth', {
-//         method: 'POST',
-//         mode: 'cors',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(credentials)
-//     });
+    let response = await fetch('auth', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    });
 
-//     if(response.status == 200) {
-//         document.getElementById('alert-msg').hidden = true;
-//         localStorage.setItem('jwt', response.headers.get('Authorization'));
-//         loadDashboard();
-//     } else {
-//         document.getElementById('alert-msg').hidden = false;
-//     }
+    if(response.status == 200) {
+        document.getElementById('alert-msg').hidden = true;
+        localStorage.setItem('jwt', response.headers.get('Authorization'));
+        loadDashboard();
+    } else {
+        document.getElementById('alert-msg').hidden = false;
+    }
 
-// }
+}
 
 // //-------------------------------------------------------------------------------------
 
@@ -69,56 +65,70 @@
 //         - register()
 // */
 
-// async function loadRegister() {
-//     console.log('in loadRegister()');
-//     APP_VIEW.innerHTML = await fetchView('register.view');
-//     DYNAMIC_CSS_LINK.href = 'css/register.css';
-//     configureRegister();
-// }
+async function loadRegister() {
+    console.log('in loadRegister()');
 
-// function configureRegister() {
-//     console.log('in configureRegister()');
-//     document.getElementById('register-username').addEventListener('blur', validateUsername);
-//     document.getElementById('register-password').addEventListener('keyup', validatePassword);
-//     document.getElementById('register-account').addEventListener('click', register);
-// }
+    // change the view to register screen
+    APP_VIEW.innerHTML = await fetchView('register.view');
+    DYNAMIC_CSS_LINK.href = 'css/register.css';
+    configureRegister(); //invoke the configureRegister()
+}
 
-// function validateUsername(event) {
-//     console.log('in validateUsername');
-//     console.log(event.target.value);
-// }
+// Needs work.....
+function configureRegister() {
+    console.log('in configureRegister()');
+    document.getElementById('register-username').addEventListener('blur', validateUsername);
+    document.getElementById('register-password').addEventListener('keyup', validatePassword);
+    document.getElementById('register-account').addEventListener('click', register);
+}
 
-// function validatePassword(event) {
-//     console.log('in validatePassword');
-//     console.log(event.target.value);
-// }
+/*
+    To validate,
+        let's think about making a call to the database for getAll usernames, 
+        make a SQL query including %LIKE% to check if there are any duplicate usernames
+*/
 
-// async function register() {
-//     console.log('in register()');
+// on blur to validate
+function validateUsername(event) {
+    console.log('in validateUsername');
+    console.log(event.target.value);
+}
 
-//     let newUser = {
-//         id: 0,
-//         username: document.getElementById('register-username').value,
-//         password: document.getElementById('register-password').value,
-//         firstName: document.getElementById('register-fn').value,
-//         lastName: document.getElementById('register-ln').value,
-//         role: {}
-//     };
+// use keyup to validate password
+function validatePassword(event) {
+    console.log('in validatePassword');
+    console.log(event.target.value);
+}
 
-//     let response = await fetch('users', {
-//         method: 'POST',
-//         mode: 'cors',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(newUser)
-//     });
 
-//     let responseBody = await response.json();
-//     console.log(responseBody);
-// }
+async function register() {
+    console.log('in register()');
 
-// //-------------------------------------------------------------------------------------
+
+// Double check, about ers_user_id and the incrementing values
+    let newUser = {
+        ers_user_id: {},
+        ers_username: document.getElementById('register-username').value,
+        ers_password: document.getElementById('register-password').value,
+        user_first_name: document.getElementById('register-fn').value,
+        user_last_name: document.getElementById('register-ln').value,
+        user_role_id: 2
+    };
+
+    let response = await fetch('users', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    });
+
+    let responseBody = await response.json();
+    console.log(responseBody);
+}
+
+//-------------------------------------------------------------------------------------
 
 // /*
 //     Dashboard component
