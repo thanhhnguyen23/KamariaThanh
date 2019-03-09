80272d71547e4d63af080876669e2489dd71536f
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-09 14:56:00 -0500

    refactoring Principal model

--- ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/models/Principal.java
+++ ExpenseReimbursementSystem/ExpenseReimbursementSystem/src/main/java/com/revature/ers/models/Principal.java
@@ -2,18 +2,31 @@ package com.revature.ers.models;
 
 public class Principal {
 	
-	private String id;
+	private int id;
 	private String role;
+	private String username; 
 	
 	public Principal() {
 		super();
 	}
 
-	public String getId() {
+	public Principal(int id, String role) {
+		super();
+		this.id = id;
+		this.role = role;
+	}
+	public Principal(int id, String role, String username) {
+		super();
+		this.id = id;
+		this.role = role;
+		this.username = username;
+	}
+
+	public int getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
@@ -25,9 +38,52 @@ public class Principal {
 		this.role = role;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Principal other = (Principal) obj;
+		if (id != other.id)
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
-		return "Principal [id=" + id + ", role=" + role + "]";
+		return "Principal [id=" + id + ", role=" + role + ", username=" + username + "]";
 	}
+	
 
 }
